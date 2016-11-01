de5fde7eae45999a8fb00caeb195293ffd0a6bf4
jetolan <jamie.tolan@gmail.com>
2016-11-01 11:26:36 -0700

    keep alive

--- reed_switch.py
+++ reed_switch.py
@@ -4,13 +4,13 @@
 
 
 
-
 #!/usr/bin/python
-
 import RPi.GPIO as GPIO
 import datetime
 import pandas as pd
 import os
+import time
+import sys
 
 pin = 6
 
@@ -20,19 +20,30 @@ def bucket_tipped(channel):
     print "rain gauge tip at : "+str(now)
 
     df=pd.DataFrame({'isotime' : pd.Series(now), 'rain_tip':pd.Series('1')})
-
-    #write to file
     
+    #write to file
     file='/home/pi/weather/rain_data.csv'
     if not os.path.exists(file):
              df.to_csv(file, header=True)
     else:
         with open(file, 'a') as f:
                       df.to_csv(f, header=False)
-
-
+       
+    
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
 GPIO.add_event_detect(pin, GPIO.FALLING, callback=bucket_tipped, bouncetime=300)
+    
+#GPIO.remove_event_detect(channel)
 
-#GPIO.remove_event_detect(channel)  
+def main_loop():
+    while 1:
+    # do your stuff...
+        time.sleep(0.1)
+
+if __name__ == '__main__':
+    try:
+        main_loop()
+    except KeyboardInterrupt:
+        print >> sys.stderr, '\nExiting by user request.\n'
+        sys.exit(0)
