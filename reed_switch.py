1ba403a73d78e42b57b6270a23564716d93b4a22
jetolan <jamie.tolan@gmail.com>
2016-11-01 10:25:00 -0700

    writes time to csv

--- reed_switch.py
+++ reed_switch.py
@@ -1,20 +1,38 @@
 
-#from:
+#based on:
 #https://www.raspberrypi.org/learning/weather-station-guide/rain-gauge.md
 
 
 
+
 #!/usr/bin/python
-  import RPi.GPIO as GPIO
 
-  pin = 6
-  count = 0
+import RPi.GPIO as GPIO
+import datetime
+import pandas as pd
+import os
 
-  def bucket_tipped(channel):
-      global count
-      count = count + 1
-      print (count * 0.2794)
+pin = 6
 
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
-  GPIO.add_event_detect(pin, GPIO.FALLING, callback=bucket_tipped, bouncetime=300)
+def bucket_tipped(channel):
+
+    now=datetime.datetime.now().isoformat()
+    print "rain gauge tip at : "+str(now)
+
+    df=pd.DataFrame({'isotime' : pd.Series(now), 'rain_tip':pd.Series('1')})
+
+    #write to file
+    
+    file='/home/pi/weather/rain_data.csv'
+    if not os.path.exists(file):
+             df.to_csv(file, header=True)
+    else:
+        with open(file, 'a') as f:
+                      df.to_csv(f, header=False)
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
+GPIO.add_event_detect(pin, GPIO.FALLING, callback=bucket_tipped, bouncetime=300)
+
+#GPIO.remove_event_detect(channel)  
