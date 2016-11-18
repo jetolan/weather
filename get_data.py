3f536052214d4216780408a1ce1046166e774c5c
jetolan <jamie.tolan@gmail.com>
2016-11-18 13:50:41 -0800

    removed mcp9808

--- get_data.py
+++ get_data.py
@@ -18,14 +18,16 @@ humidity = sensor.read_humidity()
 #timestamp = sensor.t_fine #not sure what this is...doesn't seem to be time
 
 #get data from MCP9808
-sensor = MCP9808.MCP9808()
+#sensor = MCP9808.MCP9808()
 # Initialize communication with the sensor.
-sensor.begin()
-temp = sensor.readTempC()
+#sensor.begin()
+#temp = sensor.readTempC()
 
 
 #write to dataframe
-df = pd.DataFrame({'isotime' : pd.Series(now), 'temp' : pd.Series(degrees),'temp_mcp9808' : pd.Series(temp), 'pressure' : pd.Series(pascals), 'humidity' : pd.Series(humidity)}, columns=['isotime', 'temp', 'temp_mcp9808','pressure', 'humidity'])
+#df = pd.DataFrame({'isotime' : pd.Series(now), 'temp' : pd.Series(degrees),'temp_mcp9808' : pd.Series(temp), 'pressure' : pd.Series(pascals), 'humidity' : pd.Series(humidity)}, columns=['isotime', 'temp', 'temp_mcp9808','pressure', 'humidity'])
+
+df = pd.DataFrame({'isotime' : pd.Series(now), 'temp' : pd.Series(degrees), 'pressure' : pd.Series(pascals), 'humidity' : pd.Series(humidity)}, columns=['isotime', 'temp','pressure', 'humidity'])
 
 #write to file
 file='/home/pi/weather/weather_data.csv'
