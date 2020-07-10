e1190f151c746cfe1c8de41967a4100af85b662b
Jamie Tolan <jamie.tolan@gmail.com>
2020-07-09 21:51:50 -0700

    add new adafruit lib

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup
 
 with open("README.md", 'r') as f:
-        long_description = f.read()
+    long_description = f.read()
 
-        setup(name='weather',
-                    version='0.0.1',
-                    description=u"Collect weather data from Raspberry pi",
-                    long_description=long_description,
-                    classifiers=[],
-                    keywords='',
-                    author=u"Jamie Tolan",
-                    author_email='jamie.tolan@gmail.com',
-                    url='https://github.com/jetolan/weather',
-                    license='MIT',
-                    packages=['Adafruit_Python_BME280', 'Adafruit_Python_MCP9808'],
-                    package_dir={'Adafruit_Python_BME280': 'Adafruit_Python_BME280', 'Adafruit_Python_MCP9808':'Adafruit_Python_MCP9808'},
-                    install_requires=['numpy', 'pandas']
-                    )
+    setup(name='weather',
+          version='0.0.1',
+          description=u"Collect weather data from Raspberry pi",
+          long_description=long_description,
+          classifiers=[],
+          keywords='',
+          author=u"Jamie Tolan",
+          author_email='jamie.tolan@gmail.com',
+          url='https://github.com/jetolan/weather',
+          license='MIT',
+          packages=['Adafruit_Python_BME280', 'Adafruit_Python_MCP9808'],
+          package_dir={'Adafruit_Python_BME280': 'Adafruit_Python_BME280',
+                       'Adafruit_Python_MCP9808': 'Adafruit_Python_MCP9808'},
+          install_requires=['numpy', 'pandas', 'RPI.GPIO', 'adafruit-blinka']
+          )
