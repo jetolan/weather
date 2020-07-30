fe8ca5e3677a5c2b9800a7b7d43ce6e82be864a8
Jamie Tolan <jamie.tolan@gmail.com>
2020-07-29 21:05:58 -0700

    updates for python3

--- reed_switch.py
+++ reed_switch.py
@@ -1,7 +1,6 @@
 
-#based on:
-#https://www.raspberrypi.org/learning/weather-station-guide/rain-gauge.md
-
+# based on:
+# https://www.raspberrypi.org/learning/weather-station-guide/rain-gauge.md
 
 
 #!/usr/bin/python
@@ -14,36 +13,40 @@ import sys
 
 pin = 4
 
+
 def bucket_tipped(channel):
 
-    now=datetime.datetime.now().isoformat()
-    print "rain gauge tip at : "+str(now)
+    now = datetime.datetime.now().isoformat()
+    print("rain gauge tip at : " + str(now))
 
-    df=pd.DataFrame({'isotime' : pd.Series(now), 'rain_tip':pd.Series('1')})
-    
-    #write to file
-    file='/home/pi/weather/rain_data.csv'
+    df = pd.DataFrame({'isotime': pd.Series(now), 'rain_tip': pd.Series('1')})
+
+    # write to file
+    file = '/home/pi/weather/rain_data.csv'
     if not os.path.exists(file):
-             df.to_csv(file, header=True)
+        df.to_csv(file, header=True)
     else:
         with open(file, 'a') as f:
-                      df.to_csv(f, header=False)
-       
-    
+            df.to_csv(f, header=False)
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
-GPIO.add_event_detect(pin, GPIO.FALLING, callback=bucket_tipped, bouncetime=300)
-    
-#GPIO.remove_event_detect(channel)
+GPIO.add_event_detect(
+    pin, GPIO.FALLING, callback=bucket_tipped, bouncetime=300)
+
+# GPIO.remove_event_detect(channel)
+
 
 def main_loop():
     while 1:
-    # do your stuff...
+        # do your stuff...
         time.sleep(0.1)
+
 
 if __name__ == '__main__':
     try:
         main_loop()
     except KeyboardInterrupt:
-        print >> sys.stderr, '\nExiting by user request.\n'
+        print(f'{sys.stderr}, \nExiting by user request.\n')
         sys.exit(0)
