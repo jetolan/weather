6b6c4bc2a0bd29501e9dcf15a2e16cd9b8468b89
Jamie Tolan <jamie.tolan@gmail.com>
2020-08-26 22:08:07 -0700

    add adifruit_platformdetect to install

--- setup.py
+++ setup.py
@@ -14,5 +14,7 @@ with open("README.md", 'r') as f:
           url='https://github.com/jetolan/weather',
           license='MIT',
           install_requires=['numpy', 'pandas', 'RPI.GPIO', 'adafruit-blinka',
-                            'adafruit-circuitpython-ina219', 'adafruit-circuitpython-bme280']
+                            'adafruit_platformdetect',
+                            'adafruit-circuitpython-ina219',
+                            'adafruit-circuitpython-bme280']
           )
