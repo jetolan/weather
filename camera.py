17c8216e627fb523469e444093bbc9ad9455faec
Jamie Tolan <jamie.tolan@gmail.com>
2021-02-09 19:37:16 -0800

    turn on auto exp

--- camera.py
+++ camera.py
@@ -13,31 +13,30 @@ with picamera.PiCamera() as camera:
 '''
 
 
-
-#check for output directory
-dir='/home/pi/Pictures'
+# check for output directory
+dir = '/home/pi/Pictures'
 if not os.path.exists(dir):
     os.mkdir(dir)
-    
-#get time
-now=datetime.datetime.now().isoformat()[-0:-7] 
+
+# get time
+now = datetime.datetime.now().isoformat()[-0:-7]
 
 with picamera.PiCamera() as camera:
-    #must increase GPU Memory setting to 256 on pi zero
-    camera.resolution=(4056,3040)
+    # must increase GPU Memory setting to 256 on pi zero
+    camera.resolution = (4056, 3040)
 
-    camera.framerate=Fraction(1,6)
-    camera.sensor_mode=3
-    camera.iso=800
+    camera.framerate = Fraction(1, 6)
+    camera.sensor_mode = 3
+    camera.iso = 800
 
-    #manual shutter speed max 3 seconds
-    camera.shutter_speed=int(3*1e6)
+    # manual shutter speed max 3 seconds
+    # camera.shutter_speed=int(3*1e6)
 
-    #for an auto shutter
-    #time.sleep(2)
-    #camera.shutter_speed=camera.exposure_speed
+    # for an auto shutter
+    time.sleep(2)
+    camera.shutter_speed = camera.exposure_speed
 
-    #write
-    time.sleep(5) # Camera warm-up time
+    # write
+    time.sleep(5)  # Camera warm-up time
     filename = f'{dir}/image_{now}.png'
     camera.capture(filename)
