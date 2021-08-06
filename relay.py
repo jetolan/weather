7de5f72cdf99619d461f24e5ed0ec5068b69ba7b
Jamie Tolan <jamie.tolan@gmail.com>
2021-08-06 09:23:28 -0700

    correct relay switching

--- relay.py
+++ relay.py
@@ -2,8 +2,20 @@ import RPi.GPIO as GPIO
 import datetime
 import time
 
+'''
+Use the 3.3.V pin, gnd pin and GPIO 24 to connect to the relay
+Connect COM and N0 in parallel with the well swtich
+Jumper in the L position
 
-def switch_relay():
+Grundfos Low-Level Float Switch is Normally Open in the down position
+So we have the relay normally open and in parallel
+
+Used this relay:
+https://www.amazon.com/gp/product/B00LW15A4W/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1
+'''
+
+
+def switch_relay(state):
     # get time (not used yet)
     now = datetime.datetime.now().isoformat()[-0:-7]
 
@@ -11,12 +23,20 @@ def switch_relay():
 
     RELAIS_1_GPIO = 24
     GPIO.setup(RELAIS_1_GPIO, GPIO.OUT)  # GPIO Assign mode
-    GPIO.output(RELAIS_1_GPIO, GPIO.LOW)  # out
-    GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)  # on
-    time.sleep(10)
+
+    # whether we pull up or down depends on the jumpers on the relay
+    if state == 'down':
+        GPIO.output(RELAIS_1_GPIO, GPIO.LOW)  # pull down
+    if state == 'up':
+        GPIO.output(RELAIS_1_GPIO, GPIO.HIGH)  # pull up
 
 
 if __name__ == '__main__':
     while 1:
+        # get time (not used yet)
+        now = datetime.datetime.now().isoformat()[-0:-7]
+        # will need to change time to be 15min on every hour
         time.sleep(10)
-        switch relay()
+        switch_relay('up')
+        time.sleep(10)
+        switch_relay('down')
